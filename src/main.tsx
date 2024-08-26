9283da2cca65f67cc0f94d1ab6cf656375502da6
igor modenov <madenon123gmail.com>
2024-08-26 12:51:18 +0300

    main.tsx fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App/App.tsx';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.ts';
 import SpellPage from './App/SpellPage/index.tsx';
