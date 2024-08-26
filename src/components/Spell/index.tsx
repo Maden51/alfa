05ec9d715172bdd0fe3977ba4c18d2bc54e5c4c8
igor modenov <madenon123gmail.com>
2024-08-26 12:34:21 +0300

    Вынес иконки контроля в отдельные компоненты

--- src/components/Spell/index.tsx
+++ src/components/Spell/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import WorkProps from '../../redux/services/bookApi/types';
 import styles from './styles.module.css';
 import { Link } from 'react-router-dom';
+import Like from '../Like';
+import Delete from '../Delete';
 
 function Spell(props: WorkProps) {
   const handleLike = (e: { preventDefault: () => void }) => {
@@ -19,38 +21,10 @@ function Spell(props: WorkProps) {
       <h1 className={styles.title}>{props.name}</h1>
       <div className={styles.control}>
         <button className={props.liked ? styles.liked : styles.unliked} onClick={handleLike}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill={props.liked ? 'red' : 'none'}
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-          </svg>
+          <Like liked={props.liked} />
         </button>
         <button className={styles.delete} onClick={handleDelete}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M4 7l16 0" />
-            <path d="M10 11l0 6" />
-            <path d="M14 11l0 6" />
-            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-          </svg>
+          <Delete />
         </button>
       </div>
     </Link>
