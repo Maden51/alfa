274b03a7eaef018e5740d55f2fe0b947199bb844
igor modenov <madenon123gmail.com>
2024-08-26 15:12:48 +0300

    gh-pages fix and readme updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React + TypeScript + Vite
 
 ## List of spells from magic world of Harry Potter
+
+https://maden51.github.io/alfa
--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import SpellPage from './App/SpellPage/index.tsx';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/alfa',
     element: <App />,
   },
   {
